819ba9169ba4d23487679dbfec707caad38301cf
ahmed <ahmedshalan1993@gmail.com>
2021-01-15 17:26:21 +0200

    Add admin panel pages

--- routes/web.php
+++ routes/web.php
@@ -83,6 +83,59 @@ Route::get('/player', function () {
 // end test routs
 
 
+Route::get('/admin', function () {
+    return view('admin.all_users');
+});
+
+Route::get('/admin/adduser', function () {
+    return view('admin.add_user');
+});
+
+Route::get('/admin/allusers', function () {
+    return view('admin.all_users');
+});
+
+Route::get('/admin/allcourses', function () {
+    return view('admin.all_courses');
+});
+
+Route::get('/admin/addcourse', function () {
+    return view('admin.add_course');
+});
+
+Route::get('/admin/allcategeries', function () {
+    return view('admin.all_categeries');
+});
+
+Route::get('/admin/addcategery', function () {
+    return view('admin.add_categeries');
+});
+
+
+Route::get('/admin/edituser', function () {
+    return view('admin.edit_user');
+});
+
+Route::get('/admin/editcourse', function () {
+    return view('admin.edit_course');
+});
+
+Route::get('/admin/editcategery', function () {
+    return view('admin.edit_categeries');
+});
+
+
+Route::get('/admin/viewuser', function () {
+    return view('admin.viewuser');
+});
+
+Route::get('/admin/viewcourse', function () {
+    return view('admin.viewcourse');
+});
+
+Route::get('/admin/viewcateg', function () {
+    return view('admin.viewcateg');
+});
 
 
 
